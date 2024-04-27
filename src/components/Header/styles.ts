0b3832b2909adb16b6f722e047b07a4e52502ffb
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-04-27 00:36:39 -0300

    feat: component summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,7 +6,7 @@ export const HeaderContainer = styled.header`
 `
 export const HeaderContent = styled.div`
     width: 100%;
-    max-width: 1129px;
+    max-width: 1120px;
     margin: 0 auto;
     padding: 0 1.5rem;
 
